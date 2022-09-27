8f46d647826a9c1ac6a7d3227da484e27d16801e
DaHosi69 <hosnern190218@sus.htl-grieskirchen.at>
2022-09-27 21:57:21 +0200

    Aufgabe 2

--- src/com/company/HalloJavaMitForEach.java
+++ src/com/company/HalloJavaMitForEach.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class HalloJavaMitForEach {
 
     public static void main(String[] args) {
+        List<String> stringList = new ArrayList();
+        stringList.add("Hallo");
+        stringList.add("Welt");
+        stringList.add("Kannst");
+        stringList.add("Du");
+        stringList.add("Mich");
+        stringList.add("Hören");
 
+        for(String s: stringList)
+        {
+            System.out.println(s);
+        }
+        System.out.println("\n");
+
+        stringList.forEach((String s) -> System.out.println(s));
+        System.out.println("\n");
+        stringList.forEach(System.out::println);
     }
 }
