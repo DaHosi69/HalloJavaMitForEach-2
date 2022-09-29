9881b928a4ae952f306c2ce37a56ee1e66c80cb4
DaHosi69 <hosnern190218@sus.htl-grieskirchen.at>
2022-09-29 15:39:54 +0200

    GUI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner s = new Scanner(System.in);
+        while (true)
+        {
+            System.out.println("Choose Calculator:");
+            System.out.println("1 - Rational calculator");
+            System.out.println("2 - Vector calculator");
+            System.out.println("3 - Complex Choose calculator");
+            System.out.println("4 - Exit Program");
 
+        }
     }
 }
 
